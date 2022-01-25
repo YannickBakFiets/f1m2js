c4a30e4e885f23a2bde4ce7990458bde31e456b8
yannickbuunk <yannickbuunk@hotmail.nl>
2022-01-25 09:04:33 +0100

    les 6

--- web/les-5-Silverbullet/script.js
+++ web/les-5-Silverbullet/script.js
@@ -1,66 +1,64 @@
-const MyTitle = document.getElementById("MyTitle");
-const MyImage = document.getElementById("MyImage");
-const MyInput = document.getElementById("MyInput");
+const MyTitle = document.getElementById('MyTitle');
+const MyImage = document.getElementById('MyImage');
+const MyInput = document.getElementById('MyInput');
 
-let lokaties = [
-    {
-        "titel":"plaats 0",
-        "image":"img/0.jpg"
+let locations = [{
+        "Title": "Spot 0",
+        "Image": "img/0.jpg",
     },
     {
-        "titel":"plaats 1",
-        "image":"img/1.jpg"
+        "Title": "Spot 1",
+        "Image": "img/1.jpg"
     },
     {
-        "titel":"plaats 2",
-        "image":"img/2.jpg"
+        "Title": "Spot 2",
+        "Image": "img/2.jpg"
     },
     {
-        "titel":"plaats 3",
-        "image":"img/3.jpg"
+        "Title": "Spot 3",
+        "Image": "img/3.jpg"
     },
     {
-        "titel":"plaats 4",
-        "image":"img/4.jpg"
+        "Title": "Spot 4",
+        "Image": "img/4.jpg"
     },
     {
-        "titel":"plaats 5",
-        "image":"img/5.jpg"
+        "Title": "Spot 5",
+        "Image": "img/5.jpg"
     },
     {
-        "titel":"plaats 6",
-        "image":"img/6.jpg"
+        "Title": "Spot 6",
+        "Image": "img/6.jpg"
     },
     {
-        "titel":"plaats 7",
-        "image":"img/7.jpg"
+        "Title": "Spot 7",
+        "Image": "img/7.jpg"
     },
     {
-        "titel":"plaats 8",
-        "image":"img/8.jpg"
+        "Title": "Spot 8",
+        "Image": "img/8.jpg"
     },
     {
-        "titel":"plaats 9",
-        "image":"img/9.jpg"
+        "Title": "Spot 9",
+        "Image": "img/9.jpg"
     },
     {
-        "titel":"plaats 10",
-        "image":"img/10.jpg"
+        "Title": "Spot 10",
+        "Image": "img/10.jpg"
     },
     {
-        "titel":"plaats 11",
-        "image":"img/11.jpg"
+        "Title": "Spot 11",
+        "Image": "img/11.jpg"
     }
-];
+]
 
-function show(index){
-    MyTitle.innerHTML = lokaties[index].titel;
-    MyImage.src = lokaties[index].image;
+function show(index) {
+    MyTitle.innerHTML = locations[index].Title;
+    MyImage.src = locations[index].Image;
 }
 
-function getInput(){
+function GetInput() {
     show(MyInput.value);
-    // console.log(MyInput.value)
     MyInput.value = "";
     MyInput.focus();
 }
